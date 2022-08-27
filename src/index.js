f07b8fa5f652014caadd523cc956c8024519c5c2
rimanai <109399022+rimanai@users.noreply.github.com>
2022-08-27 21:28:02 +1000

    Fetching Data from API
    Added the function that gets the temperature, wether description, wind speed and humidity from OpenWeather API.
    
    And then replaces data on my HTML page.

--- src/index.js
+++ src/index.js
@@ -1,1 +1,23 @@
+let key = "1636fb578947f1db83721ae094837fc3";
+let lat = -37;
+let lon = 144;
+let url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${key}&units=metric`;
 
+function goNow(response) {
+  console.log(response.data);
+  let innertempy = document.querySelector("#temp");
+  innertempy.innerHTML = `${Math.round(response.data.main.temp)}°`;
+  let innercity = document.querySelector("#city");
+  innercity.innerHTML = response.data.name;
+  let innercountry = document.querySelector("#country");
+  innercountry.innerHTML = response.data.sys.country;
+  let innersunny = document.querySelector("#sunny");
+  innersunny.innerHTML = response.data.weather[0].description;
+  let innerwind = document.querySelector("#wind");
+  innerwind.innerHTML = `${response.data.wind.speed} km/h`;
+  console.log(response.data.wind.speed);
+  let innnerhumid = document.querySelector("#humid");
+  innnerhumid.innerHTML = `${response.data.main.humidity}%`;
+}
+
+axios.get(url).then(goNow);
