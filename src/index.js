107031ff34e841064097d517ecd5ceea1ca59b3a
rimanai <109399022+rimanai@users.noreply.github.com>
2022-08-27 21:49:12 +1000

    Added function for Time Formatting

--- src/index.js
+++ src/index.js
@@ -3,6 +3,29 @@ let lat = -37;
 let lon = 144;
 let url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${key}&units=metric`;
 
+function formatDate(datecode) {
+  let date = new Date(datecode);
+  let hours = date.getHours();
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+  let mins = date.getMinutes();
+  if (mins < 10) {
+    mins = `0${mins}`;
+  }
+  let weekdays = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let weekday = weekdays[date.getDay()];
+  return `${weekday}, ${hours}:${mins}`;
+}
+
 function goNow(response) {
   console.log(response.data);
   let innertempy = document.querySelector("#temp");
@@ -18,6 +41,8 @@ function goNow(response) {
   console.log(response.data.wind.speed);
   let innnerhumid = document.querySelector("#humid");
   innnerhumid.innerHTML = `${response.data.main.humidity}%`;
+  let innerdate = document.querySelector("#date");
+  innerdate.innerHTML = formatDate(response.data.dt * 1000);
 }
 
 axios.get(url).then(goNow);
