9723cf624937d21a188b28e6233cfa3ed18db1ce
rimanai <109399022+rimanai@users.noreply.github.com>
2022-09-02 14:22:07 +1000

    Bug Fixing
    Updating the array so the days of the forecast won't be called 'undefined'.

--- src/index.js
+++ src/index.js
@@ -126,7 +126,18 @@ logo.addEventListener("click", handleClick);
 function formatDt(dtcode) {
   let date = new Date(dtcode * 1000);
   let day = date.getDate();
-  let daysarray = ["Thu", "Fri", "Sat", "Sun", "Mon", "Tue", "Wed"];
+  let daysarray = [
+    "Thu",
+    "Fri",
+    "Sat",
+    "Sun",
+    "Mon",
+    "Tue",
+    "Wed",
+    "Thu",
+    "Fri",
+    "Sat",
+  ];
   return daysarray[day];
 }
 
